6d64478f60ae44c95a5e143f31a0c3ed914fa61c
Phungthanhloc.2000@gmail.com <Phungthanhloc.2000@gmail.com>
2021-07-22 20:30:56 +0700

    Step 21 upload ảnh lên form

---
+++ DAChuyenNganh/Areas/Admin/Controllers/UploadController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace DAChuyenNganh.Areas.Admin.Controllers
+{
+    public class UploadController : BaseController
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public UploadController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpPost]
+        public async Task UploadImageForCKEditor(IList<IFormFile> upload, string CKEditorFuncNum, string CKEditor, string langCode)
+        {
+            DateTime now = DateTime.Now;
+            if (upload.Count == 0)
+            {
+                await HttpContext.Response.WriteAsync("Yêu cầu nhập ảnh");
+            }
+            else
+            {
+                var file = upload[0];
+                var filename = ContentDispositionHeaderValue
+                                    .Parse(file.ContentDisposition)
+                                    .FileName
+                                    .Trim('"');
+
+                var imageFolder = $@"\uploaded\images\{now.ToString("yyyyMMdd")}";
+
+                string folder = _hostingEnvironment.WebRootPath + imageFolder;
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+                string filePath = Path.Combine(folder, filename);
+                using (FileStream fs = System.IO.File.Create(filePath))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+                await HttpContext.Response.WriteAsync("<script>window.parent.CKEDITOR.tools.callFunction(" + CKEditorFuncNum + ", '" + Path.Combine(imageFolder, filename).Replace(@"\", @"/") + "');</script>");
+            }
+        }
+        /// <summary>
+        /// Tải lên hình ảnh cho form
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult UploadImage()
+        {
+            DateTime now = DateTime.Now;
+            var files = Request.Form.Files;
+            if (files.Count == 0)
+            {
+                return new BadRequestObjectResult(files);
+            }
+            else
+            {  //sử dụng head value để lấy được tên file
+                var file = files[0];
+                var filename = ContentDispositionHeaderValue
+                                    .Parse(file.ContentDisposition)
+                                    .FileName
+                                    .Trim('"');
+                //tự tổ chức thư mục
+                var imageFolder = $@"\uploaded\images\{now.ToString("yyyyMMdd")}";
+                //dùng hosting để lấy được thư mục root vs thư mục image
+                string folder = _hostingEnvironment.WebRootPath + imageFolder;
+                // kiểm tra xem thư mục đã tồn tại hay chưa
+                if (!Directory.Exists(folder))
+                {  //chưa tồn tại thì tạo mới
+                    Directory.CreateDirectory(folder);
+                }
+                string filePath = Path.Combine(folder, filename);
+                using (FileStream fs = System.IO.File.Create(filePath))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+                return new OkObjectResult(Path.Combine(imageFolder, filename).Replace(@"\", @"/"));
+            }
+        }
+    }
+}
