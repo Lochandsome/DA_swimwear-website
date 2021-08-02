5fafd582f45e83ea1fd764519c9120c133918906
Phungthanhloc.2000@gmail.com <Phungthanhloc.2000@gmail.com>
2021-08-02 14:58:59 +0700

    Step 37 : đăng nhập và đăng xuất cho khách hàng

--- DAChuyenNganh/Controllers/AccountController.cs
+++ DAChuyenNganh/Controllers/AccountController.cs
@@ -62,7 +62,7 @@ namespace DAChuyenNganh.Controllers
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
@@ -241,9 +241,9 @@ namespace DAChuyenNganh.Controllers
             {
                 _logger.LogInformation("User created a new account with password.");
 
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
-                await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
+                //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                //var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
+                //await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
 
                 await _signInManager.SignInAsync(user, isPersistent: false);
                 _logger.LogInformation("User created a new account with password.");
